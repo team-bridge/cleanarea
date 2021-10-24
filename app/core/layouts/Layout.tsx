28cc565d7b872052b5c8404f678584b179fdd379
KyoungDuckKim <skate2@naver.com>
2021-10-24 20:40:20 +0900

    style: 파비콘 로고 추가

--- app/core/layouts/Layout.tsx
+++ app/core/layouts/Layout.tsx
@@ -10,8 +10,11 @@ const Layout = ({ title, children }: LayoutProps) => {
   return (
     <>
       <Head>
-        <title>{title || "cleanarea"}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{title || "청정구역"}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       {children}
