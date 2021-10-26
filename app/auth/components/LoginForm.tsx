527abdfa82f692f844e318ed9a60f57c44319ab7
Administrator <nate.xadparo@gmail.com>
2021-10-26 09:26:41 +0900

    feat: 로그인 페이지에 레이아웃 적용

--- app/auth/components/LoginForm.tsx
+++ app/auth/components/LoginForm.tsx
@@ -1,4 +1,7 @@
 import { AuthenticationError, Link, useMutation, Routes } from "blitz"
+import Box from "@mui/material/Box"
+import Button from "@mui/material/Button"
+import { blue } from "@mui/material/colors"
 import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { Form, FORM_ERROR } from "app/core/components/Form"
 import login from "app/auth/mutations/login"
@@ -12,42 +15,76 @@ export const LoginForm = (props: LoginFormProps) => {
   const [loginMutation] = useMutation(login)
 
   return (
-    <div>
-      <h1>Login</h1>
-
-      <Form
-        submitText="Login"
-        schema={Login}
-        initialValues={{ email: "", password: "" }}
-        onSubmit={async (values) => {
-          try {
-            await loginMutation(values)
-            props.onSuccess?.()
-          } catch (error: any) {
-            if (error instanceof AuthenticationError) {
-              return { [FORM_ERROR]: "Sorry, those credentials are invalid" }
-            } else {
-              return {
-                [FORM_ERROR]:
-                  "Sorry, we had an unexpected error. Please try again. - " + error.toString(),
-              }
-            }
-          }
+    <>
+      <Box
+        sx={{
+          width: 250,
+          mt: 2,
+          mb: 2,
+          padding: 2,
+          border: `1px solid ${blue[500]}`,
+          borderRadius: 2,
         }}
       >
-        <LabeledTextField name="email" label="Email" placeholder="Email" />
-        <LabeledTextField name="password" label="Password" placeholder="Password" type="password" />
-        <div>
-          <Link href={Routes.ForgotPasswordPage()}>
-            <a>Forgot your password?</a>
-          </Link>
-        </div>
-      </Form>
-
-      <div style={{ marginTop: "1rem" }}>
-        Or <Link href={Routes.SignupPage()}>Sign Up</Link>
-      </div>
-    </div>
+        <Form
+          submit={"로그인"}
+          schema={Login}
+          initialValues={{ email: "", password: "" }}
+          onSubmit={async (values) => {
+            try {
+              await loginMutation(values)
+              props.onSuccess?.()
+            } catch (error: any) {
+              if (error instanceof AuthenticationError) {
+                return { [FORM_ERROR]: "Sorry, those credentials are invalid" }
+              } else {
+                return {
+                  [FORM_ERROR]:
+                    "Sorry, we had an unexpected error. Please try again. - " + error.toString(),
+                }
+              }
+            }
+          }}
+        >
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <LabeledTextField name="email" label="Email" placeholder="Email" />
+            <LabeledTextField
+              name="password"
+              label="Password"
+              placeholder="Password"
+              type="password"
+            />
+            <Button type="submit" size="small" variant="contained">
+              로그인
+            </Button>
+          </Box>
+        </Form>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          color: blue[900],
+        }}
+      >
+        <Link href={Routes.ForgotPasswordPage()}>
+          <Button size="small" variant="outlined" sx={{ mr: 1 }}>
+            비밀번호 찾기
+          </Button>
+        </Link>
+        <Link href={Routes.SignupPage()}>
+          <Button size="small" variant="outlined">
+            회원가입
+          </Button>
+        </Link>
+      </Box>
+    </>
   )
 }
 
