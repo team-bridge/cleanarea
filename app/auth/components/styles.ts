9d07c5eaf8f41711e20d65d65baeae358406eb61
Administrator <nate.xadparo@gmail.com>
2021-11-13 18:37:15 +0900

    feat: 인증 컴포넌트 범용 스타일 정의

---
+++ app/auth/components/styles.ts
@@ -0,0 +1,17 @@
+export const InputStyle = {
+  width: "100%",
+  border: "1px solid #1565c0",
+  fontSize: "12px",
+}
+export const FormInnerMarginStyle = {
+  margin: "5px 0px",
+}
+export const FormInnerPaddingStyle = {
+  padding: "5px 5px",
+}
+
+export const FormInnerStyle = {
+  display: "flex",
+  width: "100%",
+  ...FormInnerMarginStyle,
+}
