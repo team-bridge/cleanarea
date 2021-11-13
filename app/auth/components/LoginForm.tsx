3c9616e1f9c70348e9b36ecbb23cde6578cf7a3d
Administrator <nate.xadparo@gmail.com>
2021-11-13 18:37:38 +0900

    feat: 로그인 폼 ebay 클론 코딩

--- app/auth/components/LoginForm.tsx
+++ app/auth/components/LoginForm.tsx
@@ -6,12 +6,21 @@ import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { Form, FORM_ERROR } from "app/core/components/Form"
 import login from "app/auth/mutations/login"
 import { Login } from "app/auth/validations"
+import { FormInnerMarginStyle, FormInnerPaddingStyle, FormInnerStyle, InputStyle } from "./styles"
+import { useState } from "react"
+
+import { default as MuiLink } from "@mui/material/Link"
+import { Divider } from "@mui/material"
 
 type LoginFormProps = {
   onSuccess?: () => void
 }
 
+const PHASE_REQUIRE_EMAIL = 0
+const PHASE_REQUIRE_PASSWORD = 1
+
 export const LoginForm = (props: LoginFormProps) => {
+  const [phase, setPhase] = useState<number>(PHASE_REQUIRE_EMAIL)
   const [loginMutation] = useMutation(login)
 
   return (
@@ -24,10 +33,10 @@ export const LoginForm = (props: LoginFormProps) => {
           padding: 2,
           border: `1px solid ${blue[500]}`,
           borderRadius: 2,
+          fontSize: 10,
         }}
       >
         <Form
-          submit={"로그인"}
           schema={Login}
           initialValues={{ email: "", password: "" }}
           onSubmit={async (values) => {
@@ -52,37 +61,68 @@ export const LoginForm = (props: LoginFormProps) => {
               flexDirection: "column",
             }}
           >
-            <LabeledTextField name="email" label="Email" placeholder="Email" />
+            {phase === PHASE_REQUIRE_EMAIL ? (
+              <a>
+                청정구역에 로그인 또는&nbsp;
+                <Link href={Routes.SignupPage()}>
+                  <MuiLink>회원가입</MuiLink>
+                </Link>
+              </a>
+            ) : (
+              <MuiLink href="#" onClick={() => setPhase(PHASE_REQUIRE_EMAIL)}>
+                뒤로
+              </MuiLink>
+            )}
             <LabeledTextField
-              name="password"
-              label="Password"
-              placeholder="Password"
-              type="password"
+              name="email"
+              // type="email"
+              placeholder="이메일"
+              outerProps={{
+                style: {
+                  display: phase === PHASE_REQUIRE_EMAIL ? "unset" : "none",
+                },
+              }}
+              style={{ ...InputStyle, ...FormInnerMarginStyle, ...FormInnerPaddingStyle }}
             />
-            <Button type="submit" size="small" variant="contained">
-              로그인
+            {phase === PHASE_REQUIRE_PASSWORD ? (
+              <>
+                <LabeledTextField
+                  name="password"
+                  type="password"
+                  placeholder="비밀번호"
+                  style={{ ...InputStyle, ...FormInnerMarginStyle, ...FormInnerPaddingStyle }}
+                />
+                <a>
+                  비밀번호를 잊어버리셨습니까? &nbsp;
+                  <Link href={Routes.ForgotPasswordPage()}>
+                    <MuiLink>비밀번호 찾기</MuiLink>
+                  </Link>
+                </a>
+              </>
+            ) : null}
+            {phase === PHASE_REQUIRE_EMAIL ? (
+              <Button
+                type="button"
+                size="small"
+                variant="contained"
+                onClick={() => {
+                  setPhase(PHASE_REQUIRE_PASSWORD)
+                }}
+                style={FormInnerStyle}
+              >
+                계속
+              </Button>
+            ) : (
+              <Button type="submit" size="small" variant="contained" style={FormInnerStyle}>
+                로그인
+              </Button>
+            )}
+            <Divider>또는</Divider>
+            <Button type="button" size="small" variant="contained" style={FormInnerStyle}>
+              <Link href="#">블리자드로 로그인</Link>
             </Button>
           </Box>
         </Form>
-      </Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          justifyContent: "center",
-          color: blue[900],
-        }}
-      >
-        <Link href={Routes.ForgotPasswordPage()}>
-          <Button size="small" variant="outlined" sx={{ mr: 1 }}>
-            비밀번호 찾기
-          </Button>
-        </Link>
-        <Link href={Routes.SignupPage()}>
-          <Button size="small" variant="outlined">
-            회원가입
-          </Button>
-        </Link>
       </Box>
     </>
   )
