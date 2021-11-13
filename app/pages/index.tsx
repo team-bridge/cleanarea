8cfc14b3ac64bd09b2859918e922f8e61c02f688
Administrator <nate.xadparo@gmail.com>
2021-11-13 18:38:45 +0900

    chore: 홈에서 회원가입 <=> 로그인 버튼 위치 서로 변경

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -36,14 +36,14 @@ const UserInfo = () => {
   } else {
     return (
       <>
-        <Link href={Routes.SignupPage()}>
-          <a className="button small">
-            <strong>Sign Up</strong>
-          </a>
-        </Link>
         <Link href={Routes.LoginPage()}>
           <a className="button small">
             <strong>Login</strong>
+          </a>
+        </Link>
+        <Link href={Routes.SignupPage()}>
+          <a className="button small">
+            <strong>Sign Up</strong>
           </a>
         </Link>
       </>
